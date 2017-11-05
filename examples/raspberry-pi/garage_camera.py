f5984ff89f9f4e54f0e1136bf6c18c66484dac6e
Carlos Arrastia <carlos.arrastia@gmail.com>
2017-11-05 19:54:32 +0100

    Add nice logging and fixes to Raspberry Pi basic example

--- examples/raspberry-pi/garage_camera.py
+++ examples/raspberry-pi/garage_camera.py
@@ -10,12 +10,22 @@ Raspberry Pi
     RPI.GPIO  # backend for gpiozero
     picamera
 """
+import logging
+
 from signal import pause
 from datetime import datetime
 
 from garage_watch import CameraController
 from gpiozero import Button
 from picamera import PiCamera
+
+# logger for the script
+logger = logging.getLogger(__name__)
+
+# set up logging to print to console
+logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)
+# disable logging of transitions module
+logging.getLogger('transitions').setLevel(logging.ERROR)
 
 
 class GarageCameraController(CameraController):
@@ -25,14 +35,15 @@ class GarageCameraController(CameraController):
 
     # create the Pi camera instance and configure it
     camera = PiCamera()
-    camera.resolution = (1280, 720)
-    camera.framerate = 5
 
     def start_recording(self):
         """
         Kick off recording with the raspberry camera, it will
         use a video with filename the current timestamp
         """
+        self.camera.resolution = (1280, 720)
+        self.camera.framerate = 5
+        self.camera.start_preview()
         self.camera.start_recording(
             "{}.h264".format(datetime.now().isoformat()),
             quality=25
@@ -44,6 +55,7 @@ class GarageCameraController(CameraController):
         use a video with filename the current timestamp
         """
         self.camera.stop_recording()
+        self.camera.stop_preview()
 
     def prepare_recording(self):
         """
@@ -51,8 +63,12 @@ class GarageCameraController(CameraController):
         """
         from time import sleep
         # FIXME: something async better than a sleep
+        logger.info("Waiting 10 seconds before starting recording")
         sleep(10)
-        self.prepare_finished()
+        # if still in prepare state send the event 
+        # (might have been cancelled while waiting)
+        if self.state == 'prepare':
+            self.prepare_finished()
 
 
 def main():
@@ -60,11 +76,11 @@ def main():
     The main execution function for this script
     """
     # define the magnetic door sensor connected to pin 14
-    door_sensor = Button(14, bounce_time=0.5)
+    door_sensor = Button(14)
 
     # define the cancel momentary push button connected to pin 27
-    cancel_button = Button(27, bounce_time=0.5)
-
+    cancel_button = Button(27)
+	
     # create the camera controller instance
     cam_control = GarageCameraController()
 
@@ -73,7 +89,9 @@ def main():
     door_sensor.when_released = lambda: cam_control.door_closed()
 
     # hook cancel button pressed callbacks to the camera controller
-    cancel_button.when_pressed = lambda: cam_control.cancel_button_pressed()
+    cancel_button.when_pressed = lambda: cam_control.cancel_requested()
+
+
 
     # kick off if the door is open when the script is run
     if door_sensor.is_pressed:
